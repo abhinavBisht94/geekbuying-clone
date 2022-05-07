d1659e37fbaf9695e0b9fd87f78e0d95f038fd6f
abhinavBisht94 <98757679+abhinavBisht94@users.noreply.github.com>
2022-05-07 14:27:42 +0530

    Update AboutUS.jsx

--- geekbuying/src/components/Navbar-Footer/AboutUS.jsx
+++ geekbuying/src/components/Navbar-Footer/AboutUS.jsx
@@ -3,7 +3,60 @@ import React from "react";
 const AboutUS = () => {
   return (
     <div>
-      <div></div>
+      <div>
+        <div>
+          <h4>Customer Service</h4>
+          <p>Service Contacts</p>
+          <p>Track My Order</p>
+          <p>Return Policy</p>
+          <p>Declaration & Tracking</p>
+          <p>Why Buy From Us</p>
+          <p>Q&A</p>
+          <p>FAQs about COVID-19</p>
+        </div>
+
+        <div>
+          <h4>Product Quality</h4>
+          <p>Product Sources</p>
+          <p>Product Quality Control</p>
+          <p>Xiaomi and Mi-brand</p>
+        </div>
+
+        <div>
+          <h4>Payment & Shipping</h4>
+          <p>Payment Methods</p>
+          <p>Shipping Guide</p>
+          <p>Taxes, Customs & Duties</p>
+          <p>Verification Procedure</p>
+        </div>
+
+        <div>
+          <h4>Other Business</h4>
+          <p>Wholesale</p>
+          <p>Affiliate Program</p>
+          <p>Request a Sample</p>
+        </div>
+
+        <div>
+          <h4>About Geekbuying.com</h4>
+          <p>About Us</p>
+          <p>Our Guarantee</p>
+          <p>Terms of Use</p>
+          <p>Privacy Policy</p>
+          <p>Sitemap</p>
+        </div>
+      </div>
+
+      <div>
+        <div>
+          We are a group of students, who were given a project to clone
+          https://www.geekbuying.com/. If you have a query related to our
+          project please contact us through our linkdin, we wold be happy to
+          reply about our project.
+        </div>
+        <div></div>
+        <div></div>
+      </div>
     </div>
   );
 };
