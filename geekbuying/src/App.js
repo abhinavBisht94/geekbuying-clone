8d8a28281f78f0479015fde00e587b6f447cc31e
abhinavBisht94 <98757679+abhinavBisht94@users.noreply.github.com>
2022-05-07 15:08:13 +0530

    Update App.js

--- geekbuying/src/App.js
+++ geekbuying/src/App.js
@@ -8,9 +8,9 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      {/* <Home /> */}
-      {/* <Mobile/> */}
-      <AboutUS/>
+      <Home />
+      <Mobile/>
+      {/* <AboutUS/> */}
       <Footer />
 
     </div>
