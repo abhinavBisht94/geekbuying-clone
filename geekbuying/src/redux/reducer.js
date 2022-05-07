b10d308e11582ffa99b39577123fa8bf4587cc9f
abhinavBisht94 <98757679+abhinavBisht94@users.noreply.github.com>
2022-05-07 18:02:06 +0530

    Update reducer.js

--- geekbuying/src/redux/reducer.js
+++ geekbuying/src/redux/reducer.js
@@ -1,39 +1,39 @@
-import {GET_DATA_REQ,GET_DATA_FAIL,GET_DATA_SUCC} from './actionTypes'
+import { GET_DATA_REQ, GET_DATA_FAIL, GET_DATA_SUCC } from "./actionTypes";
 
 const initState = {
-    isLoading: false,
-    isError: false,
-    products: [],
-  };
-  
-  const reducer = (state = initState, { type, payload }) =>{
-   switch(type){
-     case GET_DATA_REQ:{
-       return {
+  isLoading: false,
+  isError: false,
+  products: [],
+};
+
+const reducer = (state = initState, { type, payload }) => {
+  switch (type) {
+    case GET_DATA_REQ: {
+      return {
         ...state,
-         isLoading:true,
-         isError:false,
-         products:[]
-       }
-     }
-     case GET_DATA_SUCC:{
-       return {
+        isLoading: true,
+        isError: false,
+        products: [],
+      };
+    }
+    case GET_DATA_SUCC: {
+      return {
         ...state,
-         isLoading:false,
-         isError:false,
-         products:payload
-       }
-     }
-     case GET_DATA_FAIL:{
-       return {
-         ...state,
-         isLoading:false,
-         isError:true,
-         products:[]
-       }
-     }
-     default: return state
-   }
-  };
-  export { reducer };
-  
+        isLoading: false,
+        isError: false,
+        products: payload,
+      };
+    }
+    case GET_DATA_FAIL: {
+      return {
+        ...state,
+        isLoading: false,
+        isError: true,
+        products: [],
+      };
+    }
+    default:
+      return state;
+  }
+};
+export { reducer };
