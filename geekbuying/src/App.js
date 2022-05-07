e114cb01ae57b26e086b778fd94c2bb5582957c3
amananshul <80920301+amananshul@users.noreply.github.com>
2022-05-07 10:04:58 +0530

    ok

--- geekbuying/src/App.js
+++ geekbuying/src/App.js
@@ -2,13 +2,15 @@ import { Home } from "./components/home/Home";
 import "./components/home/home.css";
 import Navbar from "./components/Navbar-Footer/Navbar";
 import Footer from "./components/Navbar-Footer/Footer";
-
+import Mobile from "./components/Products/Mobile"
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Home />
+      <Mobile/>
       <Footer />
+
     </div>
   );
 }
