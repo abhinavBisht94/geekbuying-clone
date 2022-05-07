5aa7f98dfd1753db03947d610aa3e59b723d4aa7
amananshul <80920301+amananshul@users.noreply.github.com>
2022-05-07 15:16:55 +0530

    Update App.js

--- geekbuying/src/App.js
+++ geekbuying/src/App.js
@@ -2,7 +2,7 @@ import { Home } from "./components/home/Home";
 import "./components/home/home.css";
 import Navbar from "./components/Navbar-Footer/Navbar";
 import Footer from "./components/Navbar-Footer/Footer";
-import Mobile from "./components/Products/Mobile"
+import {Mobile} from "./components/Products/Mobile"
 import AboutUS from "./components/Navbar-Footer/AboutUS";
 function App() {
   return (
