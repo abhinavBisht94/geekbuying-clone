c0706e7a57e97a09afe2245a95f45ec332fc35fd
snoorbasha50 <97452851+snoorbasha50@users.noreply.github.com>
2022-05-09 08:05:16 +0530

    Update App.js

--- geekbuying/src/App.js
+++ geekbuying/src/App.js
@@ -1,11 +1,13 @@
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
+import signin  from  "./components/signin";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Footer />
+      <signin/>
     </div>
   );
 }
